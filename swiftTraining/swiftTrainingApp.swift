9ef6a67937afca2e2e24de9bb78678aeebb084e8
k-isaka <isk.rugby@gmail.com>
2022-10-07 22:39:56 +0900

    Initial Commit

---
+++ swiftTraining/swiftTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftTrainingApp.swift
+//  swiftTraining
+//
+//  Created by 井坂航 on 2022/10/07.
+//
+
+import SwiftUI
+
+@main
+struct swiftTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
